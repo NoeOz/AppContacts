92af83b7072a5df723696dc464c6d2f1fec9992d
NANOTECH <redalamtaoui@gmail.com>
2020-02-10 10:15:52 +0100

    Profil design

--- Contacts/app/src/main/java/com/example/contacts/ProfileActivity.java
+++ Contacts/app/src/main/java/com/example/contacts/ProfileActivity.java
@@ -2,14 +2,19 @@ package com.example.contacts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import net.glxn.qrgen.android.QRCode;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
-import net.glxn.qrgen.android.QRCode;
+import java.util.Date;
+
 
 public class ProfileActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,15 @@ public class ProfileActivity extends AppCompatActivity {
         // Suponiendo que tienes un ImageView con el id ivCodigoGenerado
         ImageView imagenCodigo = findViewById(R.id.codeQR);
         imagenCodigo.setImageBitmap(bitmap);
+    }
+    public void dialPhoneNumber(String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+    public void appeler(View view){
+        dialPhoneNumber("01212121212");
     }
 }
